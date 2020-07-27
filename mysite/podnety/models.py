3d984caded3188f3bb051333a499cda728c85f17
SoTeKie <33070089+SoTeKie@users.noreply.github.com>
2020-07-27 20:46:25 +0200

    Switched datetime to datefield

--- mysite/podnety/models.py
+++ mysite/podnety/models.py
@@ -7,4 +7,4 @@ class Podnet(models.Model):
     adress = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     image = models.ImageField()
-    uploadDate = models.DateTimeField(auto_now_add=True)
+    uploadDate = models.DateField(auto_now_add=True)
